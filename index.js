a513113875a2947a639e30b313da7fcb53ecd91e
aquraish716 <aquraish@gmail.com>
2019-12-12 21:01:02 -0600

    updated file

--- index.js
+++ index.js
@@ -4,14 +4,27 @@ const app = express();
 
 const port = process.env.PORT || 4001;
 
+
+const employees = require('./routes/employees');
+const salary = require('./routes/salary');
+const department = require('./routes/department');
+
 const bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.get('/', (req, res) => {
-    res.send("Welcome to our API");
-})
+// app.get('/', (req, res) => {
+//     res.send("Welcome to our API");
+// })
+
+app.use(employees);
+
+app.use(salary);
+
+app.use(department);
+
+
 
 
 app.listen(port, () => {
