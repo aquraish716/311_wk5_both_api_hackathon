1aaf58c476f59ce43e4667478af310dc59146313
aquraish716 <aquraish@gmail.com>
2019-12-18 19:02:48 -0600

    updated routes and controllers files

--- controllers/employees.js
+++ controllers/employees.js
@@ -4,36 +4,36 @@ const { handleSQLError } = require('../mysql/error');
 
 const getEmployees = (req, res) => {
    //Limit the result to 50
-   let sql = 'SELECT * FROM ?? LIMIT ?';
-   sql = mysql.format(sql, ['employees', 50, req.params.id]);
+    let sql = 'SELECT * FROM ?? LIMIT ?';
+    sql = mysql.format(sql, ['employees', 50]);
     //Select employees
 pool.query(sql, (err, rows) => { 
 if (err) return handleSQLError(res, err)
     return res.json(rows);
-  });
+});
 };
 
 const getEmployeesById = (req, res) => {
     //Select employees by id
     let sql = 'SELECT * FROM ?? WHERE emp_no = ?';
-    sql = mysql.format(sql['employees', req.params.id]);
+    sql = mysql.format(sql, ['employees', req.params.emp_no]);
     pool.query(sql, (err, rows) => {
         if (err) return handleSQLError(res, err)
         return res.json(rows);
-      })
+    })
     }
 
-    const getEmployeesByFirstName = (req, res) =>{
-        let sql = 'SELECT * FROM ?? WHERE ?? = ?';
-        sql = mysql.format(sql, ['employees', 'first_name', req.body.first_name]);
+const getEmployeesByFirstName = (req, res) => {
+        let sql = 'SELECT * FROM ?? WHERE first_name = ?';
+        sql = mysql.format(sql, ['employees', req.params.first_name]);
         pool.query(sql, (err, rows) => {
-        if (err) return handleSQLError(res, err)
-        return res.json(rows);
+            if (err) return handleSQLError(res, err)
+            return res.json(rows);
         })
-    }
+}
 
 module.exports = {
-    getEmployees, 
+    getEmployees,
     getEmployeesById, 
     getEmployeesByFirstName
 }
